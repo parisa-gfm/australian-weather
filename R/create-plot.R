88e509eee0e4a7bc0a7ef002eca3995b7232df93
parisa-gfm <parisa+demo@jumpingrivers.com>
2023-03-07 10:35:20 +0000

    Change city to Brisbane

--- R/create-plot.R
+++ R/create-plot.R
@@ -9,14 +9,14 @@ df = read_csv("data/weatherAUS.csv")
 # Create a scatter plot of Humidity9am and Temp9am
 # a location
 
-city = "Sydney"
+city = "Brisbane"
 
 scatter_plot = 
 df %>%
   filter(Location == city) %>%
   ggplot(aes(x = Temp9am, y = Humidity9am)) + 
   geom_point() + 
-  ggtitle("Sydney Weather")
+  ggtitle(city)
 
 # Save the plot in graphics/
 ggsave(filename = "graphics/scatter-temp-humidity.png", plot = scatter_plot)
